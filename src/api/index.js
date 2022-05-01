f86dc22476a0c55627f4a5416c4b44690b51b999
Yukihiro1123 <murasima1123@gmail.com>
2022-05-01 15:07:58 +0900

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-study-yukihiro.herokuapp.com/",
+  baseURL: "https://study-mern-yukihiro.herokuapp.com/",
 });
 
 API.interceptors.request.use((req) => {
